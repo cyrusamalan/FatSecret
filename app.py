f58855ac20ba493f5373c22cb4849834ff5fd18a
Cyrus Amalan <cyrusamalan@Mac.attlocal.net>
2025-05-28 20:13:39 -0700

    adding

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import base64
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render sets this PORT
+    app.run(host='0.0.0.0', port=port)
 
 CORS(app)  # ✅ allows cross-origin requests
 
